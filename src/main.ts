1ad3054b198e4d8232de959a864fa0d2d60e0712
Matt Wicks <matt.wicks@gmail.com>
2023-01-15 17:40:55 +1100

    attempt to call api

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,50 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import buildUrl from 'build-url'
+import fetch from 'node-fetch'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const url = core.getInput('url')
+    if (!url) {
+      core.setFailed('Url is required to run Page Speed Insights.')
+      return
+    }
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const key = core.getInput('key')
+    if (!key) {
+      core.setFailed('API key is required to run Page Speed Insights.')
+      return
+    }
 
-    core.setOutput('time', new Date().toTimeString())
+    const queryParams = {
+      key: core.getInput('key'),
+      url: core.getInput('url'),
+      strategy: core.getInput('strategy') || 'mobile',
+      categories: (core.getMultilineInput('categories') || ['performance']).map(
+        x => x.toUpperCase()
+      )
+    }
+
+    const fullUrl = buildUrl(
+      'https://www.googleapis.com/pagespeedonline/v5/runPagespeed',
+      {
+        queryParams
+      }
+    )
+
+    core.info(`Running Page Speed Insights for ${url}`)
+    const response = await fetch(fullUrl)
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const data: any = await response.json()
+
+    core.setOutput('lighthouseResult', JSON.stringify(data.lighthouseResult))
+
+    for (const categoryKey of Object.keys(data.categories)) {
+      const category = data.categories[categoryKey]
+      const score = data.lighthouseResult.categories[category].score * 100
+      core.setOutput(category, score)
+      core.info(`${category}: ${score}`)
+    }
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
