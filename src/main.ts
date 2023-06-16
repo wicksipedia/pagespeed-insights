c6e02120d8a09ac2c72c2da88a81f27fbe75b6f5
Matt Wicks [SSW] <wicksipedia@users.noreply.github.com>
2023-06-16 12:42:31 +1000

    refactor code to separate retry concern from api call

--- src/main.ts
+++ src/main.ts
@@ -3,53 +3,27 @@ import fetch from 'node-fetch'
 
 async function run(): Promise<void> {
   try {
-    const queryParams = {
-      key: core.getInput('key', {required: true}),
-      url: core.getInput('url', {required: true}),
-      strategy: core.getInput('strategy'),
-      categories: core.getMultilineInput('categories').map(x => x.toUpperCase())
-    }
-
-    let retryCount = 0
-    const maxRetries = 3
-    const delayDuration = 20000 // 20 seconds
-    core.debug(`Query params: ${JSON.stringify(queryParams, null, 2)}`)
-
-    const url = new URL(
-      'https://www.googleapis.com/pagespeedonline/v5/runPagespeed'
-    )
-    url.searchParams.append('key', core.getInput('key', {required: true}))
-    url.searchParams.append('url', core.getInput('url', {required: true}))
-    url.searchParams.append('strategy', core.getInput('strategy'))
-    for (const category of core.getMultilineInput('categories')) {
-      url.searchParams.append('category', category.toUpperCase())
-    }
-    core.debug(`URL: ${url.toString()}`)
+    const url = getPagespeedInsightAPIUrl()
 
     core.info(`Running Page Speed Insights for ${queryParams.url}`)
-    while (retryCount < maxRetries) {
-      const response = await fetch(url.toString())
-      if (response.ok) {
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        const data: any = await response.json()
 
-        const lighthouseResult = data.lighthouseResult
-        for (const category of Object.values(lighthouseResult?.categories)) {
-          processCategory(category)
-        }
+    const maximumNumberOfRetries = +core.getInput('maximumNumberOfRetries')
+    const delayBetweenRetries = +core.getInput('delayBetweenRetries')
+    let retryCount = 0
+
+    while (retryCount < maximumNumberOfRetries) {
+      if (getLighthouseResult(url)) {
         core.info(`✅ PageSpeed Insights - Website has been analayzed successfully!`)
-        break
-      } else {
-        core.info(`⚠️ Something went wrong! Retry ${retryCount + 1}/${maxRetries} - Reattempting it in ${delayDuration/1000} secs`)
+        break;
       }
+
+      core.info(`⚠️ Something went wrong! Retry ${retryCount + 1}/${maximumNumberOfRetries} - Reattempting it in ${delayBetweenRetries/1000} secs`)
       retryCount++
 
-      if (retryCount < maxRetries) {
-        await new Promise(resolve => setTimeout(resolve, delayDuration))
+      if (retryCount < maximumNumberOfRetries) {
+        await new Promise(resolve => setTimeout(resolve, delayBetweenRetries))
       } else {
-        throw new Error(
-          '❌ PageSpeed Insights - failed to analayze the Website'
-        )
+        throw new Error('❌ PageSpeed Insights - failed to analayze the Website')
       }
     }
   } catch (error) {
@@ -59,17 +33,51 @@ async function run(): Promise<void> {
 
 run()
 
+function getPagespeedInsightAPIUrl(): URL {
+  const queryParams = {
+    key: core.getInput('key', {required: true}),
+    url: core.getInput('url', {required: true}),
+    strategy: core.getInput('strategy'),
+    categories: core.getMultilineInput('categories').map(x => x.toUpperCase())
+  }
+
+  core.debug(`Query params: ${JSON.stringify(queryParams, null, 2)}`)
+
+  const url = new URL(
+    'https://www.googleapis.com/pagespeedonline/v5/runPagespeed'
+  )
+  url.searchParams.append('key', core.getInput('key', {required: true}))
+  url.searchParams.append('url', core.getInput('url', {required: true}))
+  url.searchParams.append('strategy', core.getInput('strategy'))
+  for (const category of core.getMultilineInput('categories')) {
+    url.searchParams.append('category', category.toUpperCase())
+  }
+  core.debug(`URL: ${url.toString()}`)
+}
+
+function getLighthouseResult(url: URL): boolean {
+  const response = await fetch(url.toString())
+  if (response.ok) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const data: any = await response.json()
+
+    const lighthouseResult = data.lighthouseResult
+    for (const category of Object.values(lighthouseResult?.categories)) {
+      const title = snakeCase(category.title)
+      const score = category.score * 100
+      core.debug(`${title}: ${score}`)
+      core.setOutput(title, score)
+    }
+
+    return true
+  }
+
+  return false
+}
+
 function snakeCase(s: string): string {
   return s
     .replace(/([a-z])([A-Z])/g, '$1_$2')
     .replace(/\s+/g, '_')
     .toLowerCase()
-}
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-function processCategory(category: any): void {
-  const title = snakeCase(category.title)
-  const score = category.score * 100
-  core.debug(`${title}: ${score}`)
-  core.setOutput(title, score)
 }
